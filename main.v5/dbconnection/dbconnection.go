703fd40ae0ce2be9333fe2919bca9508c2862a7c
LuatCoi97 <mabongdem123@gmail.com>
2017-12-20 14:40:38 +0700

    Edit

--- main.v5/dbconnection/dbconnection.go
+++ main.v5/dbconnection/dbconnection.go
@@ -9,24 +9,17 @@ import (
 
 var (
 	Connect *sql.DB
-	Session *mgo.Session
 )
 
 func init() {
 	var err error
-	Connect, err = sql.Open("mysql", "root:30121996@tcp(localhost:3306)/data_walmart")
+	Connect, err = sql.Open("mysql", "hihi:beeketing@tcp(localhost:3306)/data_walmart")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 
-	Session, err = mgo.Dial("localhost:27017")
-	if err != nil {
-		fmt.Println("ERROR: ", err)
-	}
-	Session.SetMode(mgo.Monotonic, true)
 }
 
 func Close() {
 	Connect.Close()
-	Session.Close()
 }
